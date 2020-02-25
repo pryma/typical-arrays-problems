6a167b1a1503dd90ccbe0f38da9315f8921b4c4c
Maksim Pryma <Maksim.Pryma@standardbank.co.za>
2020-02-25 10:08:11 +0300

    feat: add typical arrays problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
 exports.min = function min (array) {
-  return 0;
+    let result = 0;
+    if (array == undefined || array.length == 0) {
+        return result;
+    }
+    for (let i of array) {
+        if (i < result) {
+            result = i;
+        }
+    }
+    return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let result = 0;
+    if (array == undefined || array.length == 0) {
+        return result;
+    }
+    for (let i of array) {
+        if (i > result) {
+            result = i;
+        }
+    }
+    return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let result = 0;
+    if (array == undefined || array.length == 0) {
+        return result;
+    }
+    let sum = 0;
+    for (let i of array) {
+        sum = sum + i;
+    }
+    result = sum / array.length;
+    return result;
 }
